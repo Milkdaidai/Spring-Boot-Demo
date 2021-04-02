e495a5ac5c0ee200a10ccd37c2ce7148abdf9884
caojingfeng <caojingfengcn@outlook.com>
2021-04-02 09:35:26 +0800

    接口添加备注

--- src/main/java/com/cjf/demo/ParaController.java
+++ src/main/java/com/cjf/demo/ParaController.java
@@ -9,16 +9,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 public class ParaController {
+
+    /**
+     * 测试接口
+     * @return
+     */
     @GetMapping({"/firstrequest"})
     public String firstRequest() {
         return "第一个测试接口";
     }
 
+    /**
+     * 传入参数
+     * @param num
+     * @return
+     */
     @GetMapping({"/requestPara"})
     public String requestPara(@RequestParam Integer num) {
         return "传入的参数值是：" + num;
     }
 
+    /**
+     * 通过Url的方式传入参数
+     * @param num
+     * @return
+     */
     @GetMapping({"/request/{num}"})
     public String requestUrl(@PathVariable Integer num){
         return "传入的参数是："+num;
